8fe3e1852a237f4a6a19bba58eab6e47fcc759da
tamurakenta <139750737+tamurakenta@users.noreply.github.com>
2023-07-24 21:01:24 +0900

    プルダウンの実装

--- db/migrate/20230722115052_rename_nickname_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class RenameNicknameColumnToUsers < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :users, :nickname, :name
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_24_052513) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_24_092513) do
   create_table "room_users", charset: "utf8", force: :cascade do |t|
     t.bigint "room_id", null: false
     t.bigint "user_id", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_24_052513) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
